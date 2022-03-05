37bfe981b0079f6282282204ad713eee09c6d44b
Croooook <rajatgoyal6404@gmail.com>
2022-03-05 18:23:17 +0530

    added contract abi

---
+++ script.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+const ethers = require("ethers");
+
+const dotenv = require("dotenv").config();
+
+const BigNumber = require("bignumber.js");
+const alchemy_key = `https://polygon-mainnet.g.alchemy.com/v2/9VPZHrKVkuzR6gAqaUZi6SpOlBZobRcO`;
+
